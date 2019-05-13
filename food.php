c25ea09383f3e82f703b27c1c8dc26c5cafb625c
Sina <49888093+sinakiaei@users.noreply.github.com>
2019-05-12 21:06:48 -0600

    Update food.php

--- food.php
+++ food.php
@@ -1,3 +1,5 @@
+<!-- This php file needs to run first -->
+
 <html>
 <head>	
 
@@ -6,88 +8,104 @@
 </head>
 
 <body>
+
 <?php
+	// Database Configuration
+	
 	$host='localhost';
 	$db = 'Food';
 	$username = 'postgres'; 
 	$password = 'postgres'; 
-	 
+	
 	$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+	
+	// Create a PostgreSQL database connection
 	
 	$conn = new PDO($dsn);
 	
-	$ageQueryStatement = 'SELECT DISTINCT(ages) FROM "serving" ORDER BY ages DESC'; 
+	//	Query form serving table to know about distinct age groups
+	
+	$ageQueryStatement = 'SELECT DISTINCT(ages) FROM "serving"'; 
 	$ageQuery = $conn->query($ageQueryStatement);
 	$ages = $ageQuery->fetchAll();
+	
+	//	Query form directional table to know about distinct directionals
 	
 	$dirQueryStatement = 'SELECT DISTINCT(directional) FROM "directional"'; 
 	$dirQuery = $conn->query($dirQueryStatement);
 	$dirs = $dirQuery->fetchAll();
 ?>
 
+<!-- First box: Header infortmation   -->
 <div class="container" style="margin-top:40px">
-	<div class="jumbotron"><center>
-	  <h1 class="display-4">Food Project</h1>
-	  <p class="lead">Sina Kiaei's Food Project for Dr. Liang</p></center>
-	  <hr class="my-4">
-	  <p>This project has been developed by Sina Kiaei as a fullfilment for coding project. </p>
+
+	<div class="jumbotron" align="center">
+	
+	  <h1 class="display-4"><strong>Food Project</strong></h1>	  
+	  <p class="lead">Sina Kiaei's Food Project for Dr. Liang</p>	  
+	  <hr class="my-4">	  
+	  <p>This project has been developed by Sina Kiaei as a fullfilment for coding project.</p>
+	  
 	</div>
+	
 </div>
 
-<div class="container" style="margin-top:20px">
+<!-- Second box(Includes two colums): Frist column are inputs AND second column is result  -->
+<div class="container" style="margin-top:40px">	
 	<div class="row" style="background-color:gainsboro; padding:20px;">
+		<!-- first column-->	
 		<div class="col-md-6" >
 			<h4> Individual Food Recommendation </h4>
+			<hr class="my-4">
 			
+			<!-- first drop down-->			
 			<label for="gender">Gender:</label>
 			  <select class="form-control" id="gender">
 				<option value="Male">Male</option>
 				<option value="Female">Female</option>
-			  </select>
-			  </br>
+			  </select>			 			
+			</br> 
 			
-			
+			<!--second drop down-->				
 			<label for="age">Age:</label>
 			  <select class="form-control" id="age">
 				<?php
+					// Echo age groups from serving table
 					foreach ($ages as $age)
 					{
 						echo '<option value="' . $age[0] . '">' . $age[0] . '</option>'; 
 					}
 				?>
-			  </select>
-			  </br>
-			  
-			<label for="dir">Food Option:</label>
+			  </select>						
+			</br> 
+			
+			<!--third drop down-->			
+			<label for="dir">Food Options:</label>
 			  <select class="form-control" id="dir">
 				<?php
+					//Echo directional options from directional table
 					foreach ($dirs as $dir)
 					{
 						echo '<option value="' . $dir[0] . '">' . $dir[0] . '</option>'; 
 					}
 				?>
-			  </select>
-			  
-			  </br> </br>
-			  
-			  <button type="button" class="btn btn-primary" id="submit" onclick="sendUserData()"> Submit User Data </button>
-			  
-			  <div id="result" style="padding-top:15px">
-			  
-			  </div>
-			
+			  </select>			
+			</br> 
+			<!--Submit button-->
+			<button type="button" class="btn btn-primary" id="submit" onclick="sendUserData()">Submit</button>
 		</div>
-
-		<div class="col-md-6">
-			<h4> Family Food Recommendation </h4>
+		<!-- second column-->
+		<div class="col-md-6" id="result" >
 		</div>
 	</div>
 </div>
 
 <script>
 
-function sendUserData()
-{
+// function that parse JSON file which prepared by "getfooddata.php" and print the result)
+
+function sendUserData(){
+	// Send selected items to "getfooddata.php" function
 	var str = "getfooddata.php?"; 
 	str += "gender='" + document.getElementById("gender").value + "'"; 
 	str += "&age='" + document.getElementById("age").value + "'"; 
@@ -96,17 +114,17 @@ function sendUserData()
 	console.log(str); 
 	
 	var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				// Parse the response
 				var obj = JSON.parse(this.responseText);
+				// In case there is no result for some categories
 				if(obj.length >0)
 				{
-					var resultSTR = "";
-					var resultSTR = "<span style='color:navy; font-weight:bold;'> Serving Size = ";
+					var resultSTR = "</br><span style='color:darksalmon; font-weight:bold;'></br>Based on your gender, age and  your food preferances, your serving size is: ";
 					resultSTR += obj[0];
-					resultSTR += "</span>";
-					resultSTR += "</br></br>Choose recommanded foods:</br>";
+					//resultSTR += "";
+					resultSTR += ". The following foods are also suggested for your health in this category:</span></br></br>";
 					for(i=1; i<obj.length; i=i+2)
 					{
 						resultSTR += "<span style='color:#2874A6; font-weight:bold;'>" + obj[i] + "</span> of <span style='color:#6C3483; font-weight:bold;'>" + obj[i+1] + "</span></br>";
@@ -115,18 +133,15 @@ function sendUserData()
 					document.getElementById("result").innerHTML = resultSTR;
 				}
 				else
-					document.getElementById("result").innerHTML = "<span style='color:orange; font-weight:bold;'>Sorry! Our data is not enought to answer your query.</span>";
-				
-				
-				
+					document.getElementById("result").innerHTML = "<span style='color:red; font-weight:bold;'>Sorry! Our data is not enought to answer your query.</span>";
+
 				console.log(this.responseText); 
-            }
-        };
-        xmlhttp.open("POST", str, true);
-        xmlhttp.send(); 
+			}
+		};
+		xmlhttp.open("POST", str, true);
+		xmlhttp.send(); 
 }
-	
-		
+			
 </script>
 
 </body>
