b863e960e58720e0a8f6297fe3c1523990bf6efc
Sina <49888093+sinakiaei@users.noreply.github.com>
2019-05-12 21:23:48 -0600

    Update getfooddata.php

--- getfooddata.php
+++ getfooddata.php
@@ -1,47 +1,52 @@
 <?php
 
-	$gender = $_REQUEST["gender"];
-	$age = $_REQUEST["age"];
-	$dir = $_REQUEST["dir"];
-	
-	$host='localhost';
-	$db = 'Food';
-	$username = 'postgres'; 
-	$password = 'postgres'; 
-	 
-	$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
-	
-	$conn = new PDO($dsn);
-	
-	$queryStatement = 'SELECT DISTINCT(d.fgcat_id, d.fgid, s.servings, REPLACE(f.srvg_sz, \',\', \';\'), REPLACE(f.food, \',\', \';\')) FROM "foodgroups" As fg, "foods" As f, "serving" As s, "directional" As d WHERE d.directional = ' .$dir. ' AND s.ages = ' . $age . ' AND s.gender = ' .$gender . ' AND s.fgid = d.fgid AND f.fgcat_id = d.fgcat_id'; 
-	$q = $conn->query($queryStatement);
-	$results = $q->fetchAll();
-	
-	$i=0;
-	$output = array(); 
-	foreach ($results as $result)
+// Gender, age and directional which submited by user 
+$gender = $_REQUEST["gender"];
+$age = $_REQUEST["age"];
+$dir = $_REQUEST["dir"];
+
+// Database Configuration	
+$host='localhost';
+$db = 'Food';
+$username = 'postgres'; 
+$password = 'postgres'; 
+ 
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+
+// Create a PostgreSQL database connection
+
+$conn = new PDO($dsn);
+
+//	Query for extracting desired results based on selected options in distinct form
+$queryStatement = 'SELECT DISTINCT(d.fgcat_id, d.fgid, s.servings, REPLACE(f.srvg_sz, \',\', \';\'), REPLACE(f.food, \',\', \';\')) FROM "foodgroups" As fg, "foods" As f, "serving" As s, "directional" As d WHERE d.directional = ' .$dir. ' AND s.ages = ' . $age . ' AND s.gender = ' .$gender . ' AND s.fgid = d.fgid AND f.fgcat_id = d.fgcat_id'; 
+$q = $conn->query($queryStatement);
+$results = $q->fetchAll();
+// Explode results based on "," and rearry them again.
+$i=0;
+$output = array(); 
+foreach ($results as $result)
+{
+	$resarray = explode(",", $result[0]); 
+	if($i==0)
+	{	
+		$output[$i]= $resarray[2];
+		$i++;
+		$output[$i]= str_replace('"', "",$resarray[3]);
+		$i++;
+		$output[$i]= str_replace(')',"",$resarray[4]);
+		$i++;			
+	}	
+	else
 	{
-		$resarray = explode(",", $result[0]); 
-		if($i==0)
-		{	
-			$output[$i]= $resarray[2];
-			$i++;
-			$output[$i]= str_replace('"', "",$resarray[3]);
-			$i++;
-			$output[$i]= str_replace(')',"",$resarray[4]);
-			$i++;			
-		}	
-		else
-		{
-			$output[$i]= str_replace('"', "",$resarray[3]);
-			$i++;
-			$output[$i]= str_replace(')',"",$resarray[4]);
-			$i++;
-		}
+		$output[$i]= str_replace('"', "",$resarray[3]);
+		$i++;
+		$output[$i]= str_replace(')',"",$resarray[4]);
+		$i++;
 	}
-	
-	$outputJSON = json_encode($output);
-	
-	echo $outputJSON;
+}
+// Form results as json
+$outputJSON = json_encode($output);
+
+echo $outputJSON;
 
 ?>
